8409d5da73335476699b3a94c24670bb60d7d6dd
neutral2010 <kasiwamoti5@gmail.com>
2022-01-15 20:55:44 +0900

    一通り完成しました。
    rubocop前に一旦コミット。もう少し、リファクタリングも試みたい。

--- main.rb
+++ main.rb
@@ -18,37 +18,41 @@ helpers do
 end
 
 # CRUD function and path for memo
-CONN = PG.connect( dbname: 'sinatra' )
 module DB
-  class << self
-    CONN = PG.connect( dbname: 'sinatra' )
-    def all
-      # conn = PG.connect( dbname: 'sinatra' )
-      result = CONN.exec("select* FROM memo")
+  CONN = PG.connect(dbname: 'sinatra')
 
+  class << self
+    def all
+      result = CONN.exec('select* from memo')
       memos = []
       result.each do |row|
         memos << row
       end
+      memos.each_index do |i|
+        memos[i]['id'] = memos[i]['id'].to_i
+      end
+      memos.sort_by { |v| v['id'].to_i }
     end
 
     def find(id)
-      # assert_id_format(id)
-      # parse_json(id)
-      # CONN.exec("select* FROM memo where id='4'")
+      assert_id_format(id)
+      result = CONN.exec('select* from memo where id = $1', [id])
+      get_selected_memo(result)
     end
 
-    def save(title, content)
-      conn = PG.connect( dbname: 'sinatra' )
-      # assert_id_format(memo[:id])
-      conn.prepare('sinatra', 'INSERT INTO memo (title, content) values ($1, $2)')
-      conn.exec_prepared('sinatra', [title, content])
+    def create(title, content)
+      CONN.exec('insert into memo (title, content) values ($1, $2)', [title, content])
+    end
+
+    def update(title, content, id)
+      assert_id_format(id)
+      result = CONN.exec('update memo set title = $1, content = $2 where id=$3', [title, content, id])
+      get_selected_memo(result)
     end
 
     def delete(id)
       assert_id_format(id)
-      # File.delete("./db/#{id}.json")
-      # CONN.exec("delete FROM memo where id='id'")
+      CONN.exec('delete from memo where id = $1', [id])
     end
 
     private
@@ -57,16 +61,13 @@ module DB
       raise "invalid id: #{id}" unless id =~ /\A[\w-]+\z/
     end
 
-  #   def dump_json(memo)
-  #     File.open("./db/#{memo[:id]}.json", 'w') do |file|
-  #       JSON.dump(memo, file)
-  #     end
-  #   end
-  #
-  #   def parse_json(id)
-  #     JSON.parse(File.read("./db/#{id}.json"), symbolize_names: true)
-  #   end
-  # end
+    def get_selected_memo(result)
+      memo = []
+      result.each do |row|
+        memo << row
+      end
+      @memo = memo[0]
+    end
   end
 end
 
@@ -80,59 +81,34 @@ get '/memos/new' do
 end
 
 post '/memos/:id' do
-  CONN.exec('INSERT INTO memo (title, content) VALUES ($1, $2)', [params['title'], params['content']])
-
-  # CONN.prepare('sinatra', 'INSERT INTO memo (title, content) values ($1, $2)')
-  # CONN.exec_prepared('sinatra', [params['title'], params['content']])
-  # DB.save(title, content)
+  title = params[:title]
+  content = params[:content]
+  DB.create(title, content)
   redirect '/'
 end
 
 get '/memos/:id' do
-  # @memo = DB.find(id)
-  result = CONN.exec("select* FROM memo where id = $1", [params[:id]])
-  memo = []
-  result.each do |row|
-    memo << row
-  end
-  @memo = memo[0]
+  id = params[:id]
+  @memo = DB.find(id)
   erb :show
 end
 
 get '/memos/:id/edit' do
   id = params[:id]
-  # @memo = DB.find(id)
-  result = CONN.exec("select* FROM memo where id = $1", [params[:id]])
-  memo = []
-  result.each do |row|
-    memo << row
-  end
-  @memo = memo[0]
+  @memo = DB.find(id)
   erb :edit
 end
 
 patch '/memos/:id' do
-  id = params[:id]
   title = params[:title]
   content = params[:content]
-  # @memo = DB.find(id)
-  # result = CONN.exec("update memo set title = 'つっちゃん', content = 'つっちゃん' where id=12")
-  # id部分の変数化
-  # result = CONN.exec("update memo set title = 'モコちゃん', content = 'モコちゃん' where id=$1", [params[:id]])
-  result = CONN.exec("update memo set title = $1, content = $2 where id=$3",[title, content, id])
-
-  memo = []
-  result.each do |row|
-    memo << row
-  end
-  @memo = memo[0]
-  # DB.save(memo)
+  id = params[:id]
+  DB.update(title, content, id)
   redirect("/memos/#{id}")
 end
 
 delete '/memos/:id' do
   id = params[:id]
-  # DB.delete(id)
-  CONN.exec("delete FROM memo where id = $1", [params[:id]])
+  DB.delete(id)
   redirect '/'
 end
